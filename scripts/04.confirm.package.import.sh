cd9fbe5f00ceec0ff8332520d3fbd6aa67306061
Wilfongjt <wilfongjt@gmail.com>
2023-06-22 18:35:40 -0400

    add-LbDocComments

--- scripts/04.confirm.package.import.sh
+++ scripts/04.confirm.package.import.sh
@@ -2,5 +2,6 @@
 cd ..
 
 python3 -c "from lb_lib.lb_script import LbScript; LbScript().hello_world()"
-
 python3 -c "from lb_lib.lb_recorder import LbRecorder; LbRecorder().hello_world()"
+python3 -c "from lb_lib.lb_dev_env import LbDevEnv; LbDevEnv().hello_world()"
+python3 -c "from lb_lib.lb_doc_comments import LbDocComments; LbDocComments().hello_world()"
