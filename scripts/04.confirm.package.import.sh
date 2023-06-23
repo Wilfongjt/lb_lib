05ac8769064cb6d91a4ff3774c5f8ae2cfa05a8b
Wilfongjt <wilfongjt@gmail.com>
2023-06-23 08:58:41 -0400

    add-lb_text_file_helper

--- scripts/04.confirm.package.import.sh
+++ scripts/04.confirm.package.import.sh
@@ -6,3 +6,4 @@ python3 -c "from lb_lib.lb_recorder import LbRecorder; LbRecorder().hello_world(
 python3 -c "from lb_lib.lb_dev_env import LbDevEnv; LbDevEnv().hello_world()"
 python3 -c "from lb_lib.lb_doc_comments import LbDocComments; LbDocComments().hello_world()"
 python3 -c "from lb_lib.lb_doc_folders import LbDocFolders; LbDocFolders().hello_world()"
+python3 -c "from lb_lib.lb_text_file_helper import LbTextFileHelper; LbTextFileHelper().hello_world()"
