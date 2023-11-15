f8496ea2bede302ef1bcec9bfe867e6b65d7f239
Wilfongjt <wilfongjt@gmail.com>
2023-11-15 08:17:11 -0500

    reorg

---
+++ source/lb_folders.py
@@ -0,0 +1,329 @@
+import os
+
+class ProjectString(str):
+    ##Formalize the naming of commonly used folders
+    ##* use the project string as a base eg "~/Development/organization-name/workspace-name/project-name"
+    def getClassName(self) -> str:
+        ##__Get Class Name on request__
+        ##* returns current class name
+        return self.__class__.__name__
+
+    def getOrganizationName(self):
+        ##* Return the Organization Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 1]
+        return rc
+
+    def getWorkspaceName(self):
+        ##* Return the Workspace Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 2]
+        return rc
+
+    def getProjectName(self):
+        ##* Return the Project Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 3]
+        return rc
+
+    def getDevelopmentFolder(self):
+        ##* Return the Development Folder Name
+        return '{}/Development'.format(os.environ['HOME'])
+
+    def getOrganizationFolder(self):
+        ##* Return the Organization Folder Name
+        rc = self.split('/')
+        rc = rc[0:rc.index('Development') + 2]
+        return '/'.join(rc)
+
+    def getWorkspaceFolder(self):
+        ##* Return the Workspace Folder Name
+        rc = self.split('/')
+        rc = rc[0:rc.index('Development') + 3]
+        return '/'.join(rc)
+
+    def getProjectFolder(self, subfolder=None):
+        ##* Return the Project Folder Name
+        rc = self.split('/')
+        rc = rc[0:rc.index('Development') + 4]
+        if subfolder:
+            rc.append(subfolder)
+        return '/'.join(rc)
+
+    def show(self):
+        print(self.getClassName())
+        print('    organization-name :', self.getOrganizationName())
+        print('    workspace-name    :', self.getWorkspaceName())
+        print('    project-name      :', self.getProjectName())
+        print('    dev-folder        :', self.getDevelopmentFolder())
+        print('    org-folder        :', self.getOrganizationFolder())
+        print('    workspace-folder  :', self.getWorkspaceFolder())
+        print('    project-folder    :', self.getProjectFolder())
+
+    ##<br/>
+    ##__Issues:__
+    ##* none
+    ##<br/><br/>
+'''
+class LbProjectFolder(ProjectString):
+    ##Extend ProjectString into a template
+    def __new__(self, name_value_list):
+        ##* name_value_list eg [{'name':'', 'value':''}...]
+        string_value = '{}/Development/<<WS_ORGANIZATION>>/<<WS_WORKSPACE>>/<<GH_PROJECT>>'.format(os.environ['HOME'])
+        for k in name_value_list:
+            string_value = string_value.replace(k['name'], k['value'])
+        instance = super().__new__(self, string_value)
+        return instance
+
+    ##<br/><br/>
+    ##__Issues:__
+    ##* none
+    ##<br/><br/>
+'''
+
+
+class DevelopmentString(str):
+    ##Formalize the naming of commonly used folders
+    ##* use the development string as a base eg "~/Development/organization-name/workspace-name/project-name"
+    def getClassName(self) -> str:
+        ##__Get Class Name on request__
+        ##* returns current class name
+        return self.__class__.__name__
+
+    def getOrganizationName(self):
+        ##* Return the Organization Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 1]
+        return rc
+
+    def getWorkspaceName(self):
+        ##* Return the Workspace Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 2]
+        return rc
+
+    def getProjectName(self):
+        ##* Return the Project Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 3]
+        return rc
+
+    def getDevelopmentFolder(self):
+        ##* Return the Development Folder Name
+        return '{}/Development'.format(os.environ['HOME'])
+
+    def getOrganizationFolder(self):
+        ##* Return the Organization Folder Name
+        rc = self.split('/')
+        rc = rc[0:rc.index('Development') + 2]
+        return '/'.join(rc)
+
+    def getWorkspaceFolder(self):
+        ##* Return the Workspace Folder Name
+        rc = self.split('/')
+        rc = rc[0:rc.index('Development') + 3]
+        return '/'.join(rc)
+
+    def getProjectFolder(self, subfolder=None):
+        ##* Return the Project Folder Name
+        rc = self.split('/')
+        rc = rc[0:rc.index('Development') + 4]
+        if subfolder:
+            rc.append(subfolder)
+        return '/'.join(rc)
+
+    def show(self):
+        print(self.getClassName())
+        print('organization:', self.getOrganizationName())
+
+    ##<br/>
+    ##__Issues:__
+    ##* none
+    ##<br/><br/>
+
+class FolderString(str):
+    def getName(self):
+        ##* Return the Folder Type Name
+        rc = self.split('/')
+        rc = rc[-1]
+        return rc
+
+class LbDevelopmentFolder(FolderString):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        offset = 1
+        f_str = folder_string.split('/')
+        f_str = f_str[0:f_str.index('Development')+offset]
+        f_str = '/'.join(f_str)
+        instance = super().__new__(cls, f_str)
+        return instance
+
+class LbOrganizationFolder(FolderString):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        offset = 2
+        f_str = folder_string.split('/')
+        f_str = f_str[0:f_str.index('Development') + offset]
+        f_str = '/'.join(f_str)
+        instance = super().__new__(cls, f_str)
+        return instance
+
+class LbWorkspaceFolder(FolderString):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        offset = 3
+        f_str = folder_string.split('/')
+        f_str = f_str[0:f_str.index('Development') + offset]
+        f_str = '/'.join(f_str)
+        instance = super().__new__(cls, f_str)
+        return instance
+
+class LbProjectFolder(FolderString):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        offset = 4
+        f_str = folder_string.split('/')
+        f_str = f_str[0:f_str.index('Development') + offset]
+        f_str = '/'.join(f_str)
+        instance = super().__new__(cls, f_str)
+        return instance
+
+class LbScriptsFolder(FolderString):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        offset = 4
+        f_str = folder_string.split('/')
+        f_str = f_str[0:f_str.index('Development') + offset]
+        f_str.append('scripts')
+        f_str = '/'.join(f_str)
+        instance = super().__new__(cls, f_str)
+        return instance
+
+class LbTemplateFolder(FolderString):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        offset = 4
+        f_str = folder_string.split('/')
+        f_str = f_str[0:f_str.index('Development') + offset]
+        f_str.append('source')
+        f_str.append('template')
+        f_str = '/'.join(f_str)
+        instance = super().__new__(cls, f_str)
+        return instance
+'''
+class LbWorkspaceFolder(str):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        hm = '{}'.format(os.environ['HOME'])
+        org = folder_string.split('/')
+        org = org[org.index('Development') + 1]
+        ws = folder_string.split('/')
+        ws = ws[ws.index('Development') + 2]
+        inst = '{}/Development/{}/{}'.format(hm, org, ws)
+        instance = super().__new__(cls, inst)
+        return instance
+
+    def getName(self):
+        ##* Return the Organization Name
+        rc = self.split('/')
+        rc = rc[rc.index('Development') + 2]
+        return rc
+
+class LbProjectFolder(str):
+    def __new__(cls,folder_string=None):
+        if not folder_string:
+            folder_string = os.getcwd()
+        hm = '{}'.format(os.environ['HOME'])
+        print('folder_string', folder_string)
+        org = folder_string.split('/')
+        org = org[org.index('Development') + 1]
+        ws = folder_string.split('/')
+        ws = ws[ws.index('Development') + 2]
+        prj = folder_string.split('/')
+        prj = prj[prj.index('Development') + 3]
+        inst = '{}/Development/{}/{}'.format(hm, org, ws, prj)
+        instance = super().__new__(cls, inst)
+        return instance
+
+    def getName(self):
+        ##* Return the Organization Name
+        rc = self.split('/')
+        print('rc',rc)
+        rc = rc[rc.index('Development') + 3]
+        return rc
+'''
+
+def main():
+    ##* Executes sample code
+    import os
+    from datetime import datetime
+
+    ddd = '{}/Development'.format(os.environ['HOME'])
+
+    '''
+    actual = ProjectString(os.getcwd())
+    print('ProjectString')
+    actual.show()
+
+    nv_list = [{'name': '<<WS_ORGANIZATION>>', 'value': 'temp-org'},
+               {'name': '<<WS_WORKSPACE>>', 'value': 'temp-ws'},
+               {'name': '<<GH_PROJECT>>', 'value': 'temp-api'},
+               {'name': '<<GH_USER>>', 'value': 'wilfongjt'}
+               ]
+    #actual = LbProjectFolder(nv_list)
+    '''
+    org_name = 'lyttlebit'
+    ws_name = '00_std'
+    prj_name = 'lb_lib'
+
+    expected='{}/Development'.format(os.environ['HOME'])
+    actual = LbDevelopmentFolder()
+    assert(actual == expected)
+    assert(actual.getName()=='Development')
+
+    #dd = '{}/lyttlebit'.format(dd)
+    expected='{}/Development/{}'.format(os.environ['HOME'],org_name)
+    actual = LbOrganizationFolder()
+    assert (actual == expected)
+    assert (actual.getName() == org_name)
+
+    #dd = '{}/00_std'.format(dd)
+    expected='{}/Development/{}/{}'.format(os.environ['HOME'],org_name, ws_name)
+
+    actual = LbWorkspaceFolder()
+    assert (actual == expected)
+    assert (actual.getName() == ws_name)
+
+    #dd = '{}/lb_lib'.format(dd)
+    expected='{}/Development/{}/{}/{}'.format(os.environ['HOME'],org_name, ws_name, prj_name)
+    actual = LbProjectFolder()
+    assert (actual == expected)
+    assert (actual.getName() == prj_name)
+
+    #dd = '{}/scripts'.format(dd)
+    expected='{}/Development/{}/{}/{}/scripts'.format(os.environ['HOME'],org_name, ws_name, prj_name)
+    actual = LbScriptsFolder()
+    print('actual',actual)
+    assert (actual == expected)
+    assert (actual.getName() == 'scripts')
+
+    #dd = '{}/source/template'.format(os.environ['HOME'])
+    expected='{}/Development/{}/{}/{}/source/template'.format(os.environ['HOME'],org_name, ws_name, prj_name)
+
+    print('expected', expected)
+    actual = LbTemplateFolder()
+    print('actual',actual)
+    assert (actual == expected)
+    assert (actual.getName() == 'template')
+
+    #print('dev-folder', actual)
+if __name__ == "__main__":
+    # execute as script
+    main()
--- source/lb_template.py
+++ source/lb_template.py
@@ -1,14 +1,19 @@
+import os
 
 from source.lb_text_file import LbTextFile
 from source.lb_util import LbUtil
+from source.lb_folders import LbTemplateFolder
+from source.lb_exceptions import FileNotFoundException
+
 
 class LbTemplate(LbTextFile):
     def __init__(self):
         super().__init__()
+        self.setFilename('{}.txt'.format(self.getClassName()))
         self.template_str = '''
         // created: <date-time>
         // author: <gh-user>
-        
+
         '''
 
     def load_template_string(self, folder=None, filename=None):
@@ -27,27 +32,22 @@ class LbTemplate(LbTextFile):
     def create(self, overwrite=True):
         ###### Create empty file on request
 
-        if overwrite: #and LbUtil().file_exists(self.getFolder(),self.getFilename()):
+        if overwrite:  # and LbUtil().file_exists(self.getFolder(),self.getFilename()):
             ##* delete file when env file exists and overwrite is True
             self.addStep('delete')
 
-            LbUtil().delete_file(self.getFolder(),self.getFilename())
+            LbUtil().delete_file(self.getFolder(), self.getFilename())
 
-        if LbUtil().file_exists(self.getFolder(),self.getFilename()):
+        if LbUtil().file_exists(self.getFolder(), self.getFilename()):
             ##* skip create when file_exists and overwrite is False
             self.addStep('created')
             return self
 
         ##* create file when file doesnt exist
 
-        #self.addStep('(defaults)')
-        #self.load(['{}={}'.format(k, defaults[k]) for k in defaults])
-        #self.addStep('(environment)', arrow='*')
-        self.save()
+        with open('{}/{}'.format(self.getFolder(), self.getFilename()), 'w') as f:
+            f.write(self.get_template_string())
 
-        #self.addStep('// (environment)')
-
-        self.clear()
         return self
 
     def merge(self, lst, template_str=None):
@@ -71,6 +71,12 @@ class LbTemplate(LbTextFile):
 
         return self
 
+    def validate(self):
+        super().validate()
+        # if not LbUtil().file_exists(self.getFolder(), self.getFilename()):
+        #    raise FileNotFoundException('File not found {}'.format(self.getFilename()))
+        return self
+
     def get_template_scrape_keys(self, ln):
         #### Scrape the key names from a ln of test
         ##* eg abcd<key-name>efg
@@ -78,7 +84,7 @@ class LbTemplate(LbTextFile):
         collect = False
         key = ''
         for ch in ln:
-            #print('ch',ch)
+            # print('ch',ch)
             if ch == '<':
                 collect = True
 
@@ -93,108 +99,53 @@ class LbTemplate(LbTextFile):
 
         return lst
 
-    def get_template_key_list(self, lines = None):
+    def get_template_key_list(self, lines=None):
         #### Get list of keys in a given template string
         keys = []
         if not lines:
-            lines = self.get_template_string().split('\n') # document.split()
+            lines = self.get_template_string().split('\n')  # document.split()
         for ln in lines:
             # remove duplicates
             keys = keys + [k for k in self.get_template_scrape_keys(ln) if k not in keys]
         return keys
 
     def set_template_string(self, tmpl_str):
-        self.template_str=tmpl_str
+        self.template_str = tmpl_str
         return self
 
     def get_template_string(self):
-        return self.template_str
 
-    #def clearInjections(self):
-    #    return self
-
-    def insert(self, key, block):
-
-        return self
-
-    def dep_get_template_string(self):
-        return '''
+        line_string = '''
         // created: <date-time>
         // author: <gh-user>
         '''
-def get_multi_line_example():
-    multi_line = '''
-class zbh() {    
-    line1
-    "line2"
-    'line3'
-}    
-    '''
-    #multi_line = [ln.replace('    ', '') for ln in multi_line.split('\n')]
-    #print('multi_line', multi_line)
-    #multi_line = multi_line.replace('    ', '')
-    #print('multi_line', multi_line)
-    return multi_line
+        tmpl_folder = LbTemplateFolder()
+        tmpl_file = '{}.tmpl'.format(self.getFilename())
+        print('templatfile {}/{}'.format(tmpl_folder, tmpl_file))
+        print('exits', LbUtil().file_exists(tmpl_folder, tmpl_file))
+        if LbUtil().file_exists(tmpl_folder, tmpl_file):
+            fn = '{}/{}.tmpl'.format(tmpl_folder, tmpl_file)
+            with open(fn) as f:
+                line_string = f.read()
+        print("line_string", line_string)
+        return line_string
+
 
 def main():
     import os
-    test_file = 'lb_template.js'
-    temp_api = 'test_lb_template'
-    temp_folder = '{}/Development/{}'.format(os.environ['HOME'], temp_api)
-    multi_line = '''
-    line1
-    "line2"
-    'line3'
-    '''
-    merge_list=[{'name': '<date-time>', 'value': 'testdate'},
-                {'name':'<gh-user>', 'value':'jw'}]
-    data_folder = os.getcwd()
-    #  hack a path to /data/hapi_templates
-    data_folder = data_folder.replace('/template','').replace('/source','')
-    data_folder = '{}/data/example_templates'.format(data_folder)
-
-    print('data_folder', data_folder)
+    from source.lb_folders import LbProjectFolder
+    target_folder = '{}/Development/temp-org/temp-ws/temp-prj'.format(os.environ['HOME'])
+    target_file = 'Lb'
+    print('folder', LbProjectFolder())
     actual = LbTemplate()
+    assert (actual == [])
+    print('  actual', actual)
+    actual.setFolder(target_folder, create=True)
+    actual.create()
+    actual.show()
+    actual.validate()
+    actual.assertExists()
 
-    assert(actual == [])
-    assert(actual.get_template_scrape_keys('abc<tmpl-key>defg') == ['<tmpl-key>'])
-
-    print('get_template_key_list',actual.get_template_key_list())
-    assert(actual.get_template_key_list() == ['<date-time>', '<gh-user>'])
-
-    #name_values=[{'name': '<date-time>', 'value': 'testdate'},{'name':'<gh-user>', 'value':'jw'}]
-    #print('merge',actual.merge(name_values))
-
-    actual = LbTemplate()\
-                .setFolder(temp_folder, create=True)\
-                .setFilename(test_file)\
-                .merge(merge_list, '# <date-time> <gh-user>')\
-                .create(overwrite=True) \
-                .show(terminalMsg='LbTemplate')
-
-    assert(actual.exists())
-
-    # load from file
-    test_file = 'lb_template.txt'
-    multi_line = '''
-    class zbh() {    
-        line1=['a',"b"]
-        "line2"
-        'line3'
-    }    
-        '''
-    merge_list = [{'name': '<date-time>', 'value': 'testdate'},
-                  {'name': '<gh-user>', 'value': 'jw'},
-                  {'name': '<multi-line>', 'value': multi_line},
-                  {'name': '<ml2>', 'value': multi_line},
-                  {'name': '<ml3>', 'value': multi_line}]
-    actual = LbTemplate() \
-                .load_template_string(data_folder, 'lb_template.txt.tmpl') \
-                .setFolder(temp_folder, create=True) \
-                .setFilename(test_file) \
-                .merge(merge_list) \
-                .create(overwrite=True) \
-                .show(terminalMsg='LbTemplate')
 
 if __name__ == "__main__":
     # execute as script
